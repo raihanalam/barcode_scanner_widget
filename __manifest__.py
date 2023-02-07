d6acb07254fb284676bf631e800aee7264fcb432
raihanalam <raihanhridoybd@gmail.com>
2023-02-07 17:05:13 +0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Barcode Scanner Widget',
+    'version': '1.0',
+    'category': '',
+    'sequence': 1,
+    'author': 'Raihan Alam Hridoy',
+    'summary': 'Simple Widget For Barcode Scanner Field',
+    'description': """
+
+    This module allows you to use a widget 'enter_field' to make a field automatic with a barcode scanner,
+    where your scanner will scan a value from a barcode and it will give a value in a input field and
+     this widget will hit a onchange function which is written for this field.
+
+""",
+    'depends': [],
+    'data': [
+
+        ],
+    'installable': True,
+    'assets': {
+        'point_of_sale.assets': [
+            'barcode_scanner_widget/static/src/js/**/*',
+        ],
+    },
+    'license': 'LGPL-3',
+}
---
+++ static/src/js/enter_field.js
@@ -0,0 +1,24 @@
+odoo.define('uni_pos.FieldChar', function (require) {
+    "use strict";
+
+    var FieldChar = require('web.basic_fields').FieldChar;
+    var registry = require('web.field_registry');
+
+    var FieldCharCustom = FieldChar.extend({
+        _onKeydown: function (e) {
+            if (e.which === $.ui.keyCode.ENTER) {
+                var element = document.getElementsByName(e.target.name);
+                console.log(e.target.name)
+                element[0].value = e.target.value
+                var event = new Event('change');
+                element[0].dispatchEvent(event);
+                return false;
+            }
+            this._super.apply(this, arguments);
+            //e.preventDefault();
+        },
+    });
+
+    registry.add('enter_field', FieldCharCustom);
+
+});
